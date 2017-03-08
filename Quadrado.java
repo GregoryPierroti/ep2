65a72cb3a2fb28f009d9683f59fca73d8894c3dc
GregoryPierroti <gregorypierroti@hotmail.com>
2017-03-07 21:52:12 -0300

    quadrado.java
    quadrado.java

---
+++ Quadrado.java
@@ -0,0 +1,14 @@
+public class Quadrado extends FigurasGeometricas{
+  private double tamlado;
+  public void setTamlado(){
+    this.tamlado=tamlado;
+  }
+  public double calculararea{
+   this.area=this.tamlado*tamlado;
+   return this.area;
+  }
+  public double calcularperimetro{
+    this.perimetro=this.tamlado*4;
+    return this.perimetro;
+  }
+}
