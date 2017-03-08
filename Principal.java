f79a793e824afd7dce22fc6f9fc34e1511054f72
GregoryPierroti <gregorypierroti@hotmail.com>
2017-03-07 23:21:23 -0300

    Principal.java
    Principal.java

---
+++ Principal.java
@@ -0,0 +1,24 @@
+public class Principal{
+        Quadrado q = new Quadrado();
+        Triangulo t = new Triangulo();
+        Retangulo r = new Retangulo();
+        Trapezio tp = new Trapezio();
+        q.set(4);
+        t.setBase(2);
+        t.setAltura(2);
+        r.setBase(4);
+        r.setAltura(2);
+        tp.setBasemai(4);
+        tp.setBasemen(3);
+        tp.setAltura(2);
+        System.out.println(q.calculararea());
+        System.out.println(q.calcularperimetro());
+        System.out.println(t.calculararea());
+        System.out.println(t.calcularperimetro());
+        System.out.println(c.calculararea());
+        System.out.println(c.calcularperimetro());
+        System.out.println(r.calculararea());
+        System.out.println(r.calcularperimetro());
+        System.out.println(tp.calculararea());
+        System.out.println(tp.calcularperimetro());
+}
